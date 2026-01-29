1cab99a45c55887e6f2fbf5d70baf28ecbb331f5
thaytuanly <0941072575@quangtri.edu.vn>
2026-01-29 13:06:39 +0700

    Add footer with copyright information

--- views/SpectatorView.tsx
+++ views/SpectatorView.tsx
@@ -191,6 +191,9 @@ const SpectatorView: React.FC = () => {
         </div>
       </div>
     </div>
+          <footer className="mt-20 text-slate-400 font-medium">
+        &copy; 2026 Thầy Tuấn Lý với hỗ trợ của Google AI Studio.
+      </footer>
   );
 };
 
